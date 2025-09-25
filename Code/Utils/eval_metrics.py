af3a2bfe3887d44c46fd93a52ff09f6827cb5d48
Arijit Das <dasarijitjnv@gmail.com>
2025-09-25 22:07:46 +0530

    Equip Evaluation Metrics for handeling both Binary and Multi class evaluation seamlessly

--- Code/Utils/eval_metrics.py
+++ Code/Utils/eval_metrics.py
@@ -1,43 +1,48 @@
-# Evaluation Metrics
 import numpy as np
-import torch
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import average_precision_score, balanced_accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
+from sklearn.metrics import roc_auc_score, average_precision_score
 from sklearn.preprocessing import label_binarize
+from scipy.special import softmax
 
-# Number of Errors
-def NumberOfErrors(y_actual, y_pred): # Two arrays: actual label and prediction
-    return np.sum(y_actual != y_pred)
+def NumberOfErrors(Y_true, Y_pred):
+    res = (Y_true.reshape(-1) != Y_pred.reshape(-1))
+    return int(sum(res))
 
-# accuracy
-def Accuracy(y_actual, y_pred): # Two arrays: actual label and prediction
-    return (np.sum(y_actual == y_pred)/y_actual.shape[0])*100
+def Accuracy(Y_true, Y_pred):
+    return accuracy_score(Y_true, Y_pred)
 
-# auroc
-def AUROC(y_actual, pred_logits):
-    # if np.sum(np.isnan(pred_logits)) == 0:
-    #     return roc_auc_score(y_actual, pred_logits)*100
-    # else:
-    #     return np.nan
-    # Target scores need to be probabilities for multiclass roc_auc, i.e. they should sum up to 1.0 over classes
-        exp_logits = np.exp(pred_logits - np.max(pred_logits, axis=1, keepdims=True))
-        Y_probs = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
-        return roc_auc_score(y_actual.reshape(-1), Y_probs, average="macro", multi_class='ovr')*100
+def AUROC(Y_true, Y_logits):
+    nan_index = np.where(np.isnan(Y_logits))[0]
+    if nan_index.size:
+        return np.nan
 
+    # Binary case
+    if np.unique(Y_true).shape[0] == 2:
+        return roc_auc_score(Y_true, Y_logits)
 
-# Balanced Accuracy
-def BalancedAccuracy(y_actual, y_pred):
-    return balanced_accuracy_score(y_actual, y_pred)*100
+    # Multiclass case
+    y_probs = softmax(Y_logits, axis=1)  # shape (N,C)
+    classes = np.unique(Y_true)
+    Y_true_bin = label_binarize(Y_true, classes=classes)
+    return roc_auc_score(Y_true_bin, y_probs, multi_class="ovr")
 
-# AUPRC
-def AUPRC(y_actual, pred_logits):
-    # if np.sum(np.isnan(pred_logits)) == 0:
-    #     precision_val, recall_val, _ = precision_recall_curve(y_actual, pred_logits)
-    #     return auc(recall_val, precision_val)*100
-    # else:
-    #     return np.nan
-    Y_true_bin = label_binarize(y_actual, classes=np.unique(y_actual))
-    return average_precision_score(Y_true_bin, pred_logits, average='macro')*100
+def AUPRC(Y_true, Y_logits):
+    nan_index = np.where(np.isnan(Y_logits))[0]
+    if nan_index.size:
+        return np.nan
+    
+    # Binary case
+    if np.unique(Y_true).shape[0] == 2:
+        return average_precision_score(Y_true, Y_logits)
+
+    # Multiclass case
+    Y_probs = softmax(Y_logits, axis=1)
+    classes = np.unique(Y_true)
+    y_true_bin = label_binarize(Y_true, classes=classes)
+    return average_precision_score(y_true_bin, Y_probs, average="macro")
+
+def BalancedAccuracy(Y_true, Y_pred):
+    return balanced_accuracy_score(Y_true, Y_pred)
 
 def get_all_metrics(Y_true, Y_pred, Y_logits, time_taken):
     num_errors = NumberOfErrors(Y_true, Y_pred)
